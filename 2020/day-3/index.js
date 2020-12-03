50775014b693a0e01d50b1bbd137b19014082842
Joe Devaney <jdevaney@shutterstock.com>
2020-12-03 14:46:04 +0000

    D3-Q2
    + refactor

--- 2020/day-3/index.js
+++ 2020/day-3/index.js
@@ -5,19 +5,56 @@ const X_OFFSET = 3;
 const TREE = '#';
 const OPEN = '.';
 
-const main1 = () => {
-  const data = fs.readFileSync(join(__dirname, 'input.txt'))
-    .toString()
-    .trimEnd()
-    .split('\n');
+const testData = [
+  '..##.......',
+  '#...#...#..',
+  '.#....#..#.',
+  '..#.#...#.#',
+  '.#...##..#.',
+  '..#.##.....',
+  '.#.#.#....#',
+  '.#........#',
+  '#.##...#...',
+  '#...##....#',
+  '.#..#...#.#',
+]
 
-  return data.slice(0.10).reduce((count, line, index) => {
-    const indexToCheck = (index * X_OFFSET) % (line.length);
-    if (line[indexToCheck] === TREE) return count + 1;
+const slopes = [
+  { right: 1, down: 1 },
+  { right: 3, down: 1 },
+  { right: 5, down: 1 },
+  { right: 7, down: 1 },
+  { right: 1, down: 2 },
+]
 
+const data = fs.readFileSync(join(__dirname, 'input.txt'))
+  .toString()
+  .trimEnd()
+  .split('\n');
+
+const countTrees = ({ right, down, forest = data }) => forest.reduce((count, line, index) => {
+  if (index % down !== 0) {
     return count;
-  }, 0);
+  }
+  const indexToCheck = (index / down * right) % (line.length);
+  if (line[indexToCheck] === TREE) return count + 1;
+
+  return count;
+}, 0);
+
+const main1 = () => {
+  return countTrees({ right: 3, down: 1});
+};
+
+const main2 = () => {
+  return slopes.reduce((total, slope) => total * countTrees(slope), 1);
 };
 
 console.log(main1());
+console.log(main2());
 
+// console.log(countTrees({ right: 1, down: 1, forest: testData }));
+// console.log(countTrees({ right: 3, down: 1, forest: testData }));
+// console.log(countTrees({ right: 5, down: 1, forest: testData }));
+// console.log(countTrees({ right: 7, down: 1, forest: testData }));
+// console.log(countTrees({ right: 1, down: 2, forest: testData }));
