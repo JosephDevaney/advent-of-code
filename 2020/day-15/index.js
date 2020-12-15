3248f296c0c529cbd41027127978386d2b67da49
Joe Devaney <jdevaney@shutterstock.com>
2020-12-15 20:26:53 +0000

    P2

--- 2020/day-15/index.js
+++ 2020/day-15/index.js
@@ -1,39 +1,32 @@
 const TARGET = 2020;
 
 const main1 = (data, target) => {
-  let memory = {};
+  let memory = new Map();
   let lastSpoken = null;
 
   for (let i = 0; i < target; i++) {
     if (i < data.length) {
       lastSpoken = data[i];
-      const { [lastSpoken]: spoken = [] } = memory;
-      memory = { ...memory, [lastSpoken]: [...spoken, i] };
+      const spoken = memory.get(lastSpoken) || [];
+      memory.set(lastSpoken, [...spoken, i]);
 
-      console.log(i, data[i]);
-      // lastSpoken = 0;
       continue;
     }
 
-    const { [lastSpoken]: spoken = [] } = memory;
-    // console.log(lastSpoken, memory, i);
+    const spoken = memory.get(lastSpoken) || [];
 
     let say;
     if (spoken.length > 1) {
       const [prevTime, lastTime] = spoken.slice(-2);
-      // console.log(i, lastSpoken, lastTime, spoken);
       say = lastTime - prevTime;
     } else {
       say = 0;
     }
 
-    const { [say]: sayMem = [] } = memory;
+    const sayMem = memory.get(say) || [];
 
-    // console.log(lastSpoken, memory, spoken, i);
-    memory = { ...memory, [say]: [...sayMem, i] };
+    memory.set(say, [...sayMem.slice(-1), i]);
     lastSpoken = say;
-
-    // console.log(memory);
   }
 
   return lastSpoken;
@@ -42,6 +35,5 @@ const main1 = (data, target) => {
 const testData = [0, 3, 6];
 const inputData = [12,20,0,6,1,17,7]
 
-// console.log(main1([2,1,3], 30000000));
 console.log(main1(inputData, TARGET));
-// console.log(main1(inputData, 30000000));
+console.log(main1(inputData, 30000000));
